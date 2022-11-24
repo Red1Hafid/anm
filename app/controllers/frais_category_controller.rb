485a24738e24c54bfc41bd644de8d9571609a109
meryemSekkat <meryemsekkat1991@gmail.com>
2022-11-24 09:39:57 +0100

    feat: fraiCategory show page add params to controller

--- app/controllers/frais_category_controller.rb
+++ app/controllers/frais_category_controller.rb
@@ -1,7 +1,22 @@
 class FraisCategoryController < ApplicationController
-  before_action :set_frais_category
+  before_action :set_frais_category, only: %i[ show]
+
+  def index
+    @fraisCategory = FraiCategory.all
+  end
+
+  def show
+  end
+
+  def new
+    @fraisCategory = FraiCategory.new
+  end
 
   def set_frais_category
     puts("frais")
+  end
+
+  def fraisCategory_params
+    params.require(:fraisCategory).permit(:name)
   end
 end
