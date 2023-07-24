f57e0ef0e9543aecffc845f37057f8db88ef6a40
MeryemSEKKAT <meryemsekkat1991@gmail.com>
2023-07-24 12:25:58 +0100

    feta: migrate database from mysql to pg

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,15 +15,24 @@ default_user = User.create(matricule: "S1", first_name: "admin", last_name: "adm
 
 #default_user.avatar.attach(io: File.open(Rails.root.join('app/assets/images/avatar-profile.png')),
                   #filename: 'cat.jpg')
-#default_user.send_confirmation_instructions 
+#default_user.send_confirmation_instructions
 Bank.create(user_id: 1, balance_furlough: 0.0, balance_open_additional_hour: 0, balance_open_additional_hour_off_days: 0, company_id: 1)
 
 
 
-default_user2 = User.create(matricule: "S2", first_name: "admin", last_name: "admin", email: "admin.admin@vo2group.com", password: "123456", password_confirmation: "123456", role_id: 2, is_active: true, status: 2,
+default_user2 = User.create(matricule: "S2", first_name: "admin", last_name: "admin", email: "admin.admin@askida.com", password: "123456", password_confirmation: "123456", role_id: 1, is_active: true, status: 2,
             started_at: '2021-08-10', gender: "1", family_status: "Autre", job_title: "Directeur principal", home_adress: "Av mohamed V ...", phone_number: "+2126.......", confirmed_at: DateTime.now, is_started_at_confirmed: true, company_id: 2)
 
 #default_user2.avatar.attach(io: File.open(Rails.root.join('app/assets/images/avatar-profile.png')),
                            #filename: 'cat.jpg')
 #default_user2.send_confirmation_instructions
 Bank.create(user_id: 2, balance_furlough: 0.0, balance_open_additional_hour: 0, balance_open_additional_hour_off_days: 0, company_id: 2)
+
+
+default_user2 = User.create(matricule: "S3", first_name: "admin", last_name: "admin", email: "admin.admin@vo2group.com", password: "123456", password_confirmation: "123456", role_id: 1, is_active: true, status: 2,
+                            started_at: '2021-08-10', gender: "1", family_status: "Autre", job_title: "Directeur principal", home_adress: "Av mohamed V ...", phone_number: "+2126.......", confirmed_at: DateTime.now, is_started_at_confirmed: true, company_id: 3)
+
+#default_user2.avatar.attach(io: File.open(Rails.root.join('app/assets/images/avatar-profile.png')),
+#filename: 'cat.jpg')
+#default_user2.send_confirmation_instructions
+Bank.create(user_id: 3, balance_furlough: 0.0, balance_open_additional_hour: 0, balance_open_additional_hour_off_days: 0, company_id: 3)
